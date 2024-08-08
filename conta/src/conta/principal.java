8fcb0747fbf7a5f46d97d27e023d004f70108d84
João Victor da Silva <51392582+jvsilva04@users.noreply.github.com>
2024-08-08 17:19:28 -0300

    Update principal.java

--- conta/src/conta/principal.java
+++ conta/src/conta/principal.java
@@ -15,27 +15,27 @@ public class principal {
 
 		Conta minhaConta = new Conta(nome, saldoInicial);
 		do {
-		x = Integer.parseInt(JOptionPane.showInputDialog("Escolha a função desejada: \n"+"1 - Depositar \n"+"2 - Sacar \n"+"3 - Consultar \n"+"0 - Sair"));
+		x = Integer.parseInt(JOptionPane.showInputDialog("Escolha a funÃ§Ã£o desejada: \n"+"1 - Depositar \n"+"2 - Sacar \n"+"3 - Consultar \n"+"0 - Sair"));
 
 		switch(x) {
 		case 1:
-		deposito = Double.parseDouble(JOptionPane.showInputDialog("Insira o valor de depósito: \n"));
+		deposito = Double.parseDouble(JOptionPane.showInputDialog("Insira o valor de depÃ³sito: \n"));
 
 		minhaConta.depositar(deposito);
 		break;
 		case 2:
-		saque= Double.parseDouble(JOptionPane.showInputDialog("Insira o valor de depósito: \n"));
+		saque= Double.parseDouble(JOptionPane.showInputDialog("Insira o valor do saque: \n"));
 
 		minhaConta.sacar(saque);
 		break;
 		case 3:
-		mensagem = String.format(""+"Nome da conta: %s%n"+"Saldo dispónivel: %f%n", minhaConta.getNome(),minhaConta.getSaldo());
+		mensagem = String.format(""+"Nome da conta: %s%n"+"Saldo dispÃ³nivel: %f%n", minhaConta.getNome(),minhaConta.getSaldo());
 
 		JOptionPane.showMessageDialog(null, mensagem);
 
 		break;
 		case 0:
-			fim= String.format("Desenvolvido por João Victor da Silva %n", null);
+			fim= String.format("Desenvolvido por JoÃ£o Victor da Silva %n", null);
 			JOptionPane.showMessageDialog(null, fim);
 		break;
 		}
